2044792bd2ca1d9e9776c208c774fb446c6efea7
stanleypapaya <stanleychenxingyu@gmail.com>
2018-11-29 13:48:08 +0800

    所有的VC完成

--- resources/js/message.js
+++ resources/js/message.js
@@ -1,53 +1,69 @@
 
-var APP_ID = '0vWwvnMlj5M28NdF1TA8cx9K-gzGzoHsz';
-var APP_KEY = 'nWChS3uQtX1LIXWn8MAC52ur';
-
-AV.init({
-  appId: APP_ID,
-  appKey: APP_KEY
-});
-
-$(document).ready(function(){
-  $('#messagePostForm').on('submit', function(e){
-    e.preventDefault()
-    let content = $('input[name=message]')[0].value
-    let name = $('input[name=name]')[0].value
-    var Message = AV.Object.extend('Message')
-    var message = new Message()
-    message.save({
-      'name' : name,
-      'content' : content
-    })
-
-    .then(function(object){
-      if(object.attributes.name && object.attributes.content){
+!function(){
+  var view = $('.message')
+  var controller = {
+    view: null,
+    init: function(view){
+      this.view = view
+      this.initAV()
+      this.loadMessage()
+      this.bindEvents()
+    },
+    initAV: function(){
+      var APP_ID = '0vWwvnMlj5M28NdF1TA8cx9K-gzGzoHsz';
+      var APP_KEY = 'nWChS3uQtX1LIXWn8MAC52ur';
+      
+      AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+      });
+    },
+    loadMessage: function(){
+      var query = new AV.Query('Message');
+      query.find().then(function (messages) {
+        let arr = messages.map(item => item.attributes)
         let messageList = $('#messageList')
-        let li = $('<li>'+ object.attributes.name  + ' : ' + object.attributes.content +'</li>')
-        messageList.append(li)
-        $('input[name=message]')[0].value = ''
-        $('input[name=name]')[0].value = ''
-      } else{
-        alert('请重新输入')
-        $('input[name=message]')[0].value = ''
-        $('input[name=name]')[0].value = ''
-      }
-    })
-  })
-})
+        arr.forEach(function(item){
+          if(item.name && item.content){
+            let li = $('<li>'+ item.name  + ' : ' + item.content +'</li>')
+            messageList.append(li)
+          }
+        })
+      }, function (error) {
+        alert('留言失败，请改天再来')
+      });
+    },
+    bindEvents: function(){
+      $(document).ready(function(){
+        $('#messagePostForm').on('submit', function(e){
+          e.preventDefault()
+          let content = $('input[name=message]')[0].value
+          let name = $('input[name=name]')[0].value
+          var Message = AV.Object.extend('Message')
+          var message = new Message()
+          message.save({
+            'name' : name,
+            'content' : content
+          }).then(function(object){
+            if(object.attributes.name && object.attributes.content){
+              let messageList = $('#messageList')
+              let li = $('<li>'+ object.attributes.name  + ' : ' + object.attributes.content +'</li>')
+              messageList.append(li)
+              $('input[name=message]')[0].value = ''
+              $('input[name=name]')[0].value = ''
+            } else{
+              alert('请重新输入')
+              $('input[name=message]')[0].value = ''
+              $('input[name=name]')[0].value = ''
+            }
+          })
+        })
+      })
+    }
+  } 
+  controller.init(view)
+}.call()
 
-var query = new AV.Query('Message');
-  query.find().then(function (messages) {
-    let arr = messages.map(item => item.attributes)
-    let messageList = $('#messageList')
-    arr.forEach(function(item){
-      if(item.name && item.content){
-        let li = $('<li>'+ item.name  + ' : ' + item.content +'</li>')
-        messageList.append(li)
-      }
-    })
-  }, function (error) {
-    alert('留言失败，请改天再来')
-  });
 
 // // 创建 TestObject表
 // var TestObject = AV.Object.extend('TestObject');
